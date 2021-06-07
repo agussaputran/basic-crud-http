722d624c196f52efa10bc9293337a2364a1446fd
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-06-07 11:16:37 +0700

    feat: edit and delete api

--- main.go
+++ main.go
@@ -6,18 +6,19 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-var db = []Data{}
+var db = []data{}
 
-type Data struct {
+type data struct {
 	ID        uint32 `json:"id"`
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
 	Age       uint16 `json:"age"`
 }
 
-type Response struct {
+type response struct {
 	Code   uint16      `json:"code"`
 	Status string      `json:"status"`
 	Data   interface{} `json:"data"`
@@ -28,17 +29,17 @@ func main() {
 	// ROUTER
 	http.HandleFunc("/get", getApi)
 	http.HandleFunc("/post", postApi)
-	// http.HandleFunc("/put", putApi)
-	// http.HandleFunc("/delete", deleteApi)
+	http.HandleFunc("/put", putApi)
+	http.HandleFunc("/delete", deleteApi)
 	log.Println("Server up and run on localhost:9000...")
 	http.ListenAndServe(":9000", nil)
 }
 
 func writeResponse(w http.ResponseWriter, r *http.Request, data interface{}, err error) {
-	var response Response
+	var res response
 
 	if err != nil {
-		response = Response{
+		res = response{
 			Code:   http.StatusInternalServerError,
 			Status: fmt.Sprint(err),
 			Data:   data,
@@ -47,26 +48,31 @@ func writeResponse(w http.ResponseWriter, r *http.Request, data interface{}, err
 	}
 
 	if err == nil {
-		response = Response{
+		res = response{
 			Code:   http.StatusOK,
 			Status: "Ok!",
 			Data:   data,
 		}
 	}
 
-	respByte, _ := json.Marshal(response)
+	respByte, _ := json.Marshal(res)
 	w.Write(respByte)
 
 }
 
 func getApi(w http.ResponseWriter, r *http.Request) {
-	writeResponse(w, r, db, nil)
+	if r.Method == "GET" {
+		writeResponse(w, r, db, nil)
+	} else {
+		w.WriteHeader(405)
+		fmt.Fprintln(w, "Method Not Allowed")
+	}
 }
 
 func postApi(w http.ResponseWriter, r *http.Request) {
-	var dataReq Data
 
 	if r.Method == "POST" {
+		var dataReq data
 
 		dataByte, _ := ioutil.ReadAll(r.Body)
 		err := json.Unmarshal(dataByte, &dataReq)
@@ -81,7 +87,51 @@ func postApi(w http.ResponseWriter, r *http.Request) {
 
 		writeResponse(w, r, dataReq, err)
 	} else {
-		fmt.Fprintln(w, "badrequest")
-		writeResponse(w, r, nil, fmt.Errorf("Bad Request"))
+		w.WriteHeader(405)
+		fmt.Fprintln(w, "Method Not Allowed")
+	}
+}
+
+func putApi(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "PUT" || r.Method == "PATCH" {
+		var dataReq data
+		dataByte, _ := ioutil.ReadAll(r.Body)
+		err := json.Unmarshal(dataByte, &dataReq)
+		if err != nil {
+			log.Println("Error on decode data", err)
+			writeResponse(w, r, nil, err)
+			return
+		}
+
+		q := r.URL.Query()
+
+		for i, v := range db {
+			idReq, _ := strconv.Atoi(q.Get("id"))
+			if v.ID == uint32(idReq) {
+				db[i].FirstName = dataReq.FirstName
+				db[i].LastName = dataReq.LastName
+				db[i].Age = dataReq.Age
+			}
+		}
+
+	} else {
+		w.WriteHeader(405)
+		fmt.Fprintln(w, "Method Not Allowed")
+	}
+}
+
+func deleteApi(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "DELETE" {
+		q := r.URL.Query()
+		idReq, _ := strconv.Atoi(q.Get("id"))
+		for i, v := range db {
+			if v.ID == uint32(idReq) {
+				db[i] = db[len(db)-1]
+				db = db[:len(db)-1]
+			}
+		}
+	} else {
+		w.WriteHeader(405)
+		fmt.Fprintln(w, "Method Not Allowed")
 	}
 }
